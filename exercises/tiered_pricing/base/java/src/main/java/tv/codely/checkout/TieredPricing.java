c0e03f60fb596f837f1659d240c0ad5552da931d
droidsam1 <droidsam@gmail.com>
2022-09-08 21:59:32 +0200

    refactor: move building tiered prices ranges to a function

--- exercises/tiered_pricing/base/java/src/main/java/tv/codely/checkout/TieredPricing.java
+++ exercises/tiered_pricing/base/java/src/main/java/tv/codely/checkout/TieredPricing.java
@@ -7,15 +7,6 @@ import java.util.List;
 
 public class TieredPricing {
 
-    public final TierPrice TIER_1_PRICE = new TierPrice(1, 2, 299);
-
-    public final TierPrice TIER2_PRICE = new TierPrice(3, 10, 239);
-
-    public final TierPrice TIER3_PRICE = new TierPrice(11, 25, 219);
-
-    public final TierPrice TIER4_PRICE = new TierPrice(26, 50, 199);
-
-
     public int getTotalPriceFor(int numberOfLicenses) {
 
         int total = 0;
@@ -29,15 +20,23 @@ public class TieredPricing {
 
     private int getUnitPriceForRange(int numberOfLicenses) {
 
-
-        for (TierPrice tierPrice : Arrays.asList(TIER_1_PRICE, TIER2_PRICE, TIER3_PRICE, TIER4_PRICE)){
-            if(tierPrice.isEligibleFor(numberOfLicenses)){
+        for (TierPrice tierPrice : getTieredPrices()) {
+            if (tierPrice.isEligibleFor(numberOfLicenses)) {
                 return tierPrice.getPrice();
             }
         }
 
         return 199;
 
+    }
+
+    private List<TierPrice> getTieredPrices() {
+        final TierPrice tier1Price = new TierPrice(1, 2, 299);
+        final TierPrice tier2Price = new TierPrice(3, 10, 239);
+        final TierPrice tier3Price = new TierPrice(11, 25, 219);
+        final TierPrice tier4Price = new TierPrice(26, 50, 199);
+
+        return Arrays.asList(tier1Price, tier2Price, tier3Price, tier4Price);
     }
 
 
