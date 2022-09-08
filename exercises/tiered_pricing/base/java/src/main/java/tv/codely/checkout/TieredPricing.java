e4ccda0a457eb0aa5efc2546a34a3fa3506efc9b
droidsam1 <droidsam@gmail.com>
2022-09-08 20:00:06 +0200

    refactor TieredPricing: expose duplicity

--- exercises/tiered_pricing/base/java/src/main/java/tv/codely/checkout/TieredPricing.java
+++ exercises/tiered_pricing/base/java/src/main/java/tv/codely/checkout/TieredPricing.java
@@ -12,11 +12,11 @@ public class TieredPricing {
         }
 
         if (numberOfLicenses == 11) {
-            return 2729;
+            return 2510 + 219;
         }
 
         if (numberOfLicenses == 12) {
-            return 2729 + 219;
+            return 2510 + 219 + 219;
         }
 
         return (2 * basePrice) + (numberOfLicenses - 2) * 239;
